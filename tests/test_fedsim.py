61fc7cea26f737a0395e9613bc7e8ca1f9a6b1e4
Farshid Varno <fr.varno@gmail.com>
2022-06-22 13:49:31 -0400

    updat tests

--- tests/test_fedsim.py
+++ tests/test_fedsim.py
@@ -3,10 +3,11 @@ from torch.utils.tensorboard import SummaryWriter
 from fedsim.distributed.centralized.training.fedavg import FedAvg
 from fedsim.distributed.data_management.basic_data_manager import BasicDataManager
 from fedsim.models.mcmahan_nets import cnn_cifar100
+from fedsim.scores import accuracy
 
 
 def test_main():
-    n_clients = 100
+    n_clients = 1000
 
     dm = BasicDataManager("./data", "cifar100", n_clients)
     sw = SummaryWriter()
@@ -17,10 +18,15 @@ def test_main():
         sample_scheme="uniform",
         sample_rate=0.01,
         model_class=cnn_cifar100,
-        epochs=5,
+        epochs=1,
         loss_fn="ce",
         batch_size=32,
         metric_logger=sw,
         device="cpu",
     )
-    assert alg.train(rounds=1) == 0
+
+    alg.hook_global_score_function("test", "accuracy", accuracy)
+    for key in dm.get_local_splits_names():
+        alg.hook_local_score_function(key, "accuracy", accuracy)
+
+    assert alg.train(rounds=1) is not None
