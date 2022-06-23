eed24b58141be10eac584ff57179b390127657f5
Farshid Varno <fr.varno@gmail.com>
2022-06-22 23:33:15 -0400

    minor change in build docs

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 # black --line-length 79 .
-isort .
 python setup.py bdist_wheel sdist
-
--- docs_build.sh
+++ docs_build.sh
@@ -2,5 +2,6 @@
 cd docs
 sphinx-apidoc -o ./source ..
 rm ./source/setup.rst
+rm ./source/scripts.rst
+make clean
 make html
-
