36b537204da862e54eaf0d2d6aa4ca78d2d72d4e
Farshid Varno <fr.varno@gmail.com>
2022-07-08 00:12:18 -0400

    deleted sphinxext as not required

--- fedsim/__init__.py
+++ fedsim/__init__.py
@@ -16,13 +16,4 @@ __all__ = [
     if module != "__init__.py" and module[-3:] == ".py"
 ]
 
-from fedsim import distributed as distributed
-from fedsim import local as local
-from fedsim import models as models
-from fedsim import datasets as datasets
-
-
-__all__ += ['distributed', 'local', 'models', 'datasets']
-
-
-
+__all__ += ["distributed", "local", "models", "datasets"]
