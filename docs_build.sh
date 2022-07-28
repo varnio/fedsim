bdda6633548ad6f65ac17b2c314ecdf46a23abdb
Farshid Varno <fr.varno@gmail.com>
2022-07-28 18:38:25 -0400

    improved doc build automation

--- docs_build.sh
+++ docs_build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd docs
-sphinx-apidoc -fMeT ../fedsim -o ./source/reference -d 1
-python rst_process.py
+sphinx-apidoc -fMeT ../fedsim -o ./source/reference -d 1 -t ./source/_templates/apidoc
 make clean
 make html
